54bd33c79b8bba4a7dc126ee422eebbfc1bb1bd2
CrazySean007 <shuaim@usc.edu>
2020-09-06 15:57:29 -0700

    create router and directory structure

---
+++ src/pages/dashboard/index.js
@@ -0,0 +1,11 @@
+import React, {Component} from "react";
+import {BrowserRouter, Route} from 'react-router-dom';
+export default class Admin extends Component {
+    render() {
+        return (
+            <BrowserRouter>
+                <Route path = '' component = {}></Route>
+            </BrowserRouter>
+        )
+    }
+}
